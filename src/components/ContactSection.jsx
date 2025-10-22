2ebc9b90192af3d7f3e11a1dd833008bdb9cbd12
student <student@students-MacBook-Pro.local>
2025-10-23 08:38:27 +1300

    @contactSection created and half built, message form to be completed

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,9 +1,137 @@
+import { Github, Instagram, Linkedin, Mail, Map, MapPin, Phone, Twitch, Twitter } from 'lucide-react'
 import React from 'react'
 
 export const ContactSection = () => {
-  return (
-    <section>
-        
-    </section>
-  )
+    return (
+        <section
+            id='contact'
+            className='py-24 px-4 relative bg-secondary/30'
+
+        >
+            <div
+                className='container mx-auto max-w-5xl'
+            >
+                <h2
+                    className='text-3xl md:text-4xl font-bold mb-4 text-center'
+                >
+                    Get In <span className='text-primary'> Touch</span>
+                </h2>
+
+                <p
+                    className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'
+                >
+                    Got a project, question, or just want to say hi? 
+                    Feel free to reach out—I’d love to hear from you. 
+                    You can send me a message using the form above or connect via email or social media.
+                </p>
+                <div
+                    className='grid grid-cols-1 md:grid-cols-2 gap-12'
+                >
+                    <div
+                        className='space-y-8'
+                    >
+                        <h3 className='text-2xl font-semibold mb-6 '> Contact Information</h3>
+                        <div className='space-y-6 justify-center'>
+                            <div
+                                className='flex items-start  space-x-4'
+                            >
+                                <div
+                                    className='p-3 rounded-full bg-primary/10'
+                                >
+                                    <Mail className='h-6 w-6 text-primary'/>
+                                </div>
+                                <div>
+                                    <h4 className='font-medium '> Email</h4>
+                                    <a 
+                                        href="mailto:birdsallAlexander@gmail.com"
+                                        className='text-muted-foreground hover:text-primary transition-colors'
+                                    >
+                                        birdsallAlexander@gmail.com
+                                    </a>
+                                </div>
+                            </div>
+                            <div
+                                className='flex items-start space-x-4'
+                            >
+                                <div
+                                    className='p-3 rounded-full bg-primary/10'
+                                >
+                                    <Phone  className='h-6 w-6 text-primary'/>
+                                </div>
+                                <div>
+                                    <h4 className='font-medium'> Phone</h4>
+                                    
+                                    <a 
+                                        href="tel:12334556 554 "
+                                        className='text-muted-foreground hover:text-primary transition-colors'
+                                    >
+                                        12334556 554
+                                    </a>
+                                </div>
+                            </div>
+                            <div
+                                className='flex items-start space-x-4'
+                            >
+                                <div
+                                    className='p-3 rounded-full bg-primary/10'
+                                >
+                                    <MapPin className='h-6 w-6 text-primary'/>
+                                </div>
+                                <div>
+                                    <h4 className='font-medium'> Location</h4>
+                                    <a 
+                                        className='text-muted-foreground hover:text-primary transition-colors'
+                                    >
+                                        Auckland, New Zealand
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div
+                            className='pt-8'
+                        >
+                            <h4
+                                className='font-medium mb-4'
+                            >
+                                Conect With Me
+                            </h4>
+                            <div
+                                className='flex space-x-4 justify-center'
+                            >
+                                <a href="#" target='_blank'>
+                                    <Linkedin />
+                                </a>
+                                <a href="#" target='_blank'>
+                                    <Twitter />
+                                </a>
+                                <a href="#" target='_blank'>
+                                    <Twitch />
+                                </a>
+                                <a href="#" target='_blank'>
+                                    <Github />
+                                </a>
+                                <a href="#" target='_blank'>
+                                    <Instagram />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div
+                           className='bg-card p-8 rounded-lg shadow-xs' 
+                        >
+                            <h3 className='text-2xl font-semibold mb-6'> Send a Message</h3>
+                            <form 
+                                action=""
+                            >
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+    )
 }
